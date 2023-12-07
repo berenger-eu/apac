bf35be680ab4c95bacfa3940baeb0b99564f18fb
GAUPP Julien <julien.gaupp@inria.fr>
2023-12-07 15:02:54 +0000

    Pointers support

--- tests/test.cpp
+++ tests/test.cpp
@@ -46,17 +46,27 @@ void cycle1(int&a,int&b)
 		cycle1(c,d);
 	}
 
+void pointersModif(int *  a)
+{
+	int b=1+*a++;
+}
+
 int main() {
   int n=1,m=3,iho;
-  int* n2=&n;
-	int* r=&n;
+	int* n2=&n;
+	
   int est_const=5;
   int est_const2=6;
 	//fonction_test(n2,m,n,r,est_const,est_const2);
 	int rec1=0;
 	int rec2=0;
 	int rec3=0;
+	int* r=&rec3;
 	int rec4=5;
+	int pointedValue=2;
+	int * pointerTo=&pointedValue;
+	int t=0;
+	pointersModif(pointerTo);
 	recurs(rec1,rec2,rec3,rec4);
 	//fonction_test2(m);
 	return 0;
