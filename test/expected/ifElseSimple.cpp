748b99963777cccf614df8f94ff62898dee5c52c
Julien Gaupp <julien.gaupp@etu.unistra.fr>
2024-03-08 15:08:05 +0100

    corrected test ifElseSimple

--- test/expected/ifElseSimple.cpp
+++ test/expected/ifElseSimple.cpp
@@ -1,27 +1,20 @@
 int main() {
-  int *const apacMemeBloc_n_0 = new int();
-  int &n = *(apacMemeBloc_n_0);
+  int *const apacMemeBloc__n_0 = new int();
+  int &n = *(apacMemeBloc__n_0);
 
   if (true) {
     int *const apacMemeBloc__m_0 = new int();
     int &m = *(apacMemeBloc__m_0);
-
-    delete apacMemeBloc_n_0;
     delete apacMemeBloc__m_0;
   } else if (true) {
     int *const apacMemeBloc__m_1 = new int();
     int &m = *(apacMemeBloc__m_1);
-
-    delete apacMemeBloc_n_0;
     delete apacMemeBloc__m_1;
-  }
-
-  else {
+  } else {
     int *const apacMemeBloc__m_2 = new int();
     int &m = *(apacMemeBloc__m_2);
-
-    delete apacMemeBloc_n_0;
     delete apacMemeBloc__m_2;
   }
-  delete apacMemeBloc_n_0;
+  delete apacMemeBloc__n_0;
+  return 1;
 }
