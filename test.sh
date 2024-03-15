9abafd2c89843f5f7ba884df73b39436523fa142
Julien <julien.gaupp@etu.unistra.fr>
2024-03-15 16:48:18 +0000

    added script to run all test suites

---
+++ test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+BOLD='\033[1m'
+
+countPassed=0
+countTotal=0
+
+# Iterate over all .cpp files in the current directory
+bash Constify/tests/autotests/test.sh
+if [ $? -ne 0 ]; then
+    echo -e "${RED}${BOLD}FAILURE: Test suite for Constify transformation${NC}"
+else
+    ((countPassed++))
+    echo -e "${GREEN}${BOLD}SUCCESS: Test suite for Constify transformation${NC}"
+fi
+((countTotal++))
+bash Heapify/tests/test.sh
+if [ $? -ne 0 ]; then
+    echo -e "${RED}${BOLD}FAILURE: Test suite for Constify transformation${NC}"
+else
+    ((countPassed++))
+    echo -e "${GREEN}${BOLD}SUCCESS: Test suite for Constify transformation${NC}"
+fi
+((countTotal++))
+echo -e "${BLUE}${BOLD}Test suite passed : $countPassed/$countTotal ${NC}"
+if [ $countPassed != $countTotal ]; then
+    exit 1
+fi
+exit 0
+done
