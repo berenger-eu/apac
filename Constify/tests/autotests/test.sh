0ddbeeb5398f5358090d98a24662f2b3729d7ab0
Julien <julien.gaupp@etu.unistra.fr>
2024-03-15 16:47:49 +0000

    corrected constify test script

--- Constify/tests/autotests/test.sh
+++ Constify/tests/autotests/test.sh
@@ -8,33 +8,42 @@ BOLD='\033[1m'
 difference=false
 countPassed=0
 countTotal=0
+
+curPath="$(realpath $(dirname "$0"))"
+constify="$curPath/../../../build/constify"
+expectedPath="$curPath/../expected"
+resultPath="$curPath/results"
+testsPath="$curPath"
+
+
+
 # Iterate over all .cpp files in the current directory
-for file in *.cpp; do
+for file in $testsPath/*.cpp; do
     # Check if the file exists and is a regular file
     if [ -f "$file" ]; then
-        echo "Processing file: $file"
         ((countTotal++))
         differenceInText=false
         differenceInAST=false
         foldername=$(basename "$file" .cpp)
-        ../../build/apac $file > /dev/null 2> /dev/null
+        $constify $file > /dev/null 2> /dev/null
         if [ $? -ne 0 ]; then
             echo -e "${RED}${BOLD}Failed to process file : $file"
             differenceInAST=true
             differenceInText=true
-        else
-            for file2 in "$foldername"/*; do
+        else      
+            for file2 in "$curPath/$foldername"/*; do
                 if [ -f "$file2" ] && [[ "$file2" != *"astdiff"* ]]; then
-                    #echo "Created file : $file2"
                     filename=$(basename "$file2" /)
-                    diff "$file2" "expected/$foldername"_expected/"$filename" > /dev/null
+                    clang-format -i "$file2"
+                    diff "$file2" "$expectedPath/$foldername"_expected/"$filename" > /dev/null
                     if [ $? -ne 0 ]; then
                         differenceInText=true
                         echo -e "${RED}Different Text : $filename${NC}"
                     fi
-                    clang-check -ast-print "expected/$foldername"_expected/"$filename" > ./ast_result 2> /dev/null
-                    clang-check -ast-print $file2 > ./ast_expected 2> /dev/null
-                    diff ast_expected ast_result > "$foldername"/astdiff
+                    
+                    clang-check -ast-print "$expectedPath/$foldername"_expected/"$filename" > $curPath/ast_result 2> /dev/null
+                    clang-check -ast-print $file2 > $curPath/ast_expected 2> /dev/null
+                    diff $curPath/ast_expected $curPath/ast_result > "$curPath/$foldername/astdiff"
                     if [ $? -ne 0 ]; then
                         differenceInAST=true
                         echo -e "${RED}Different AST : $filename${NC}"
@@ -42,9 +51,10 @@ for file in *.cpp; do
                     
                 fi
             done
-            rm ./ast_expected
-            rm ./ast_result
+            rm $curPath/ast_expected
+            rm $curPath/ast_result
         fi 
+        
         if $differenceInAST; then
             echo -e "${RED}${BOLD}Test failed, AST : $foldername${NC}"
         else 
@@ -56,9 +66,11 @@ for file in *.cpp; do
         else 
             echo -e "${GREEN}Test succeeded, TEXT : $foldername${NC}"
         fi
+        
         if [ $differenceInAST == false ] && [ $differenceInText == false ]; then
             rm -rf "$foldername"/
         fi
+        
     fi
 done
 echo -e "${BLUE}${BOLD}Tests passed : $countPassed/$countTotal ${NC}"
