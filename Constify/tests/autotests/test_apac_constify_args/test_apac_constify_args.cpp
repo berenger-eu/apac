7fe1f60a73b6311f46b6a02b329a568fcefdad44
Julien Gaupp <julien.gaupp@etu.unistra.fr>
2024-03-15 14:29:33 +0100

    Preparation for merge of repo

---
+++ Constify/tests/autotests/test_apac_constify_args/test_apac_constify_args.cpp
@@ -0,0 +1,16 @@
+typedef int * pINT;
+
+using ppINT = int**;
+
+struct A {};
+
+void e(const int  a) {}
+
+void f(const int  a) {}
+
+// TODO: Be able to use anonymous arguments, e.g. void g(float *, double &, ...)
+void g(const float *const  a, const double &  b, const int *const *const &  c) {}
+
+void h(const int *const  a, const int *const *const  b) {}
+
+void i(const struct A &  a) {}
