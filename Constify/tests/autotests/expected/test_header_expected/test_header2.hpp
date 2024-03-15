b266fb22cb73199786e95e552a306609b168c549
Julien <julien.gaupp@etu.unistra.fr>
2024-03-15 16:50:37 +0000

    removal of old expected files

--- Constify/tests/autotests/expected/test_header_expected/test_header2.hpp
+++
@@ -1,2 +0,0 @@
-int fun55( const int   bztekp);
-int izdajoij( const int   aizodjozaiozad);
--- Constify/tests/autotests/expected/test_system_variable_expected/test_system_variable.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-//To see if stderr breaks the program or not
-int main()
-{
-  const int  v=5;
-  fprintf(stderr, "Error: array is not sorted\n");
-}
