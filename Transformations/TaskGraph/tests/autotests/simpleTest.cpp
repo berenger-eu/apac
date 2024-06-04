29378440138fde266be060d65e76dab5176245e5
Julien <julien.gaupp@etu.unistra.fr>
2024-06-04 14:25:14 +0000

    testfile

--- Transformations/TaskGraph/tests/autotests/simpleTest.cpp
+++ Transformations/TaskGraph/tests/autotests/simpleTest.cpp
@@ -1,9 +1,81 @@
+//   #include <memory>
+//  #include <vector> 
+class Point{
+public:
+    Point() =default;
+    void setX(int t)
+    {
+        x = 4;
+    }
+    private:
+    int x;
+    int y;
+};
+/*
+int gun(int& b)
+{
+    int i;
+    i=4;
+    i++;
+    if(2)
+    {
+        i++;
+        i=4;
+    }
+    else if(3)
+    {
+        i++;
+        i=4;
+    }
+    else if(2)
+    {
+         b=5;
+        int b;
+        b=0;
+        b++;
+    }
+    b=5;
+    b=i;
+    b+=4;
+    int j;
+    j=4*5+4;
+    return 1;
+}
+
+int fun(int& b)
+{
+    int i;
+    i=4;
+    i++;
+    gun(b);
+    b=i;
+    return gun(b);
+}*/
+ /*
+void ptrtest(std::shared_ptr<int> ptr,int &k,int* p)
+{
+    *ptr = 4;
+    k++;
+    *p=4;
+}*/
 int main()
 {
     int i;
     i=4;
+    
     int j;
+  //  std::shared_ptr<int> ptr;
+   // ptr = std::make_shared<int>(i);
+   // ptr.get();
+   // *ptr = 4;
     j=i;
+    i++;
+    Point po;
+    po.setX(4);
+    po.setX(8);
+    po.setX(i);
+    po.setX(j);
+    // ptrtest(ptr,j,&i);
     /*
     for(int k=0;k<4;k++)
     {
@@ -20,7 +92,8 @@ int main()
         }
         int a;
         a++;
-    }*/
+    }
+    
     i=41;
     j++;
     i=4;
@@ -31,7 +104,6 @@ int main()
     }
     j=4;
     i=4+i+j;
-    int a;
-    a=i+j;
+  */  
     return 0;
 }
