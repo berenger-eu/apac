3517d61dd4264ac22eface52201cd64785d0d7fc
Julien <julien.gaupp@etu.unistra.fr>
2024-07-08 14:42:06 +0000

    struct used to store variables

--- Transformations/TaskGraph/include/AliasTable.hpp
+++ Transformations/TaskGraph/include/AliasTable.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "ElementsRepresentationStruct.hpp"
 #include "common.hpp"
 #include "clang/AST/Decl.h"
 #include "clang/Rewrite/Core/Rewriter.h"
@@ -33,35 +34,6 @@ struct referenceAliasArg : public aliasArg {
   // Element referenced
   std::unordered_set<aliasArg *> aliased;
   void dump() const override;
-};
-
-struct key_struct {
-  const clang::NamedDecl *decl;
-  std::vector<int> indices;
-  void dump() const {
-    // Dump the contents of the key_struct
-    llvm::errs() << decl->getNameAsString();
-    for (const auto &index : indices) {
-      llvm::errs() << " [" << index << "]";
-    }
-    llvm::errs() << "\n";
-  }
-};
-struct key_hash {
-  std::size_t operator()(const key_struct p) const {
-    auto h1 = std::hash<const clang::NamedDecl *>{}(p.decl);
-    auto h2 = 0;
-    for (const auto &element : p.indices) {
-      h2 ^= std::hash<int>{}(element);
-    }
-    return h1 ^ h2;
-  }
-};
-
-struct key_equal {
-  bool operator()(const key_struct p1, key_struct p2) const {
-    return p1.decl == p2.decl; //&& p1.indices == p2.indices;
-  }
 };
 
 typedef std::unordered_map<key_struct, struct aliasArg, key_hash, key_equal>
