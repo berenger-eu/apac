16eb6b5fa898b86acaab1c2b5f2724bccfef042e
Julien <julien.gaupp@etu.unistra.fr>
2024-07-10 12:49:19 +0000

    function count and changes in calls

--- Transformations/TaskGraph/include/AliasTable.hpp
+++ Transformations/TaskGraph/include/AliasTable.hpp
@@ -37,7 +37,7 @@ using IndexTableMap = std::map<int, aliasesTableValues>;
 struct IndexTableMapStruct {
   IndexTableMap map;
   // current alias, for if map corresponds to tab[1][x] then alias is tab[1]
-  aliasArg alias;
+  aliasArg *alias;
   aliasesTableValues *at(const std::vector<int> &indexes) {
     // If no indexes, return nullptr
     if (indexes.empty())
@@ -57,6 +57,36 @@ struct IndexTableMapStruct {
     // index, so we return nullptr
     else
       return nullptr;
+  }
+  const aliasesTableValues *at(const std::vector<int> &indexes) const {
+    // If no indexes, return nullptr
+    if (indexes.empty())
+      return nullptr;
+    // If only one index, return the value
+    if (indexes.size() == 1 && map.count(indexes[0]))
+      return &map.at(indexes[0]);
+    // If the value is an IndexTableMapStruct, we look through it using indexes
+    else if (map.count(indexes[0])) {
+      if (std::holds_alternative<std::shared_ptr<IndexTableMapStruct>>(
+              map.at(indexes[0])))
+        return std::get<std::shared_ptr<IndexTableMapStruct>>(
+                   map.at(indexes[0]))
+            ->at(std::vector<int>(indexes.begin() + 1, indexes.end()));
+    }
+    // Otherwise, the value is simple variable and we're trying to access an
+    // index, so we return nullptr
+    else
+      return nullptr;
+  }
+  int count(const std::vector<int> &indexes) const {
+    if (indexes.empty())
+      return 0;
+    if (indexes.size() == 1)
+      return (map.count(indexes[0]) > 0);
+    if (map.count(indexes[0]) == 0)
+      return 0;
+    return std::get<std::shared_ptr<IndexTableMapStruct>>(map.at(indexes[0]))
+        ->count(std::vector<int>(indexes.begin() + 1, indexes.end()));
   }
 };
 struct AliasTableMapStruct;
@@ -80,6 +110,36 @@ struct AliasTableMapStruct {
     else
       return std::get<std::shared_ptr<IndexTableMapStruct>>(map.at(key))
           ->at(indexes);
+  }
+  const aliasesTableValues *
+  at(const NamedDecl *key,
+     const std::vector<int> &indexes = std::vector<int>()) const {
+    // If no entry for variable, return nullptr
+    if (map.count(key) == 0)
+      return nullptr;
+    // If no indexes, return the value
+    if (indexes.empty()) {
+      map.at(key);
+    }
+    // If the value is an aliasArg (and not an array, so no indexes), return
+    // nullptr
+    else if (std::holds_alternative<aliasArg *>(map.at(key)))
+      return nullptr;
+    // The value is an array, so we look through it using indexes
+    else
+      return std::get<std::shared_ptr<IndexTableMapStruct>>(map.at(key))
+          ->at(indexes);
+  }
+  int count(const NamedDecl *key,
+            const std::vector<int> &indexes = std::vector<int>()) const {
+    if (indexes.empty())
+      return map.count(key);
+    else if (map.count(key) != 0) {
+      if (std::holds_alternative<aliasArg *>(map.at(key)))
+        return 0;
+      return std::get<std::shared_ptr<IndexTableMapStruct>>(map.at(key))
+          ->count(indexes);
+    }
   }
 };
 class AliasTable {
@@ -110,7 +170,14 @@ public:
     const NamedDecl *key = getKey(v);
     if (aliasTableMap.count(key) == 0)
       return nullptr;
-    return &aliasTableMap.at(key).at(std::vector<int>());
+    if (std::holds_alternative<aliasArg *>(*aliasTableMap.at(key)))
+      return std::get<aliasArg *>(*aliasTableMap.at(key));
+    else {
+      // TODO: Remove this later (if no issues arrise)
+      llvm::errs() << "Error in getAliasArgConst\n";
+      v->dump();
+      return nullptr;
+    }
   }
 
 private:
@@ -126,9 +193,9 @@ private:
     if (v != nullptr)
       return;
     if (aliasTableMap.count(key) == 0)
-      aliasTableMap.at(key).insert({indexes, aliasArg(*v, Variable, indexes)});
-    else if (aliasTableMap.at(key).count(indexes) == 0)
-      aliasTableMap.at(key).insert({indexes, aliasArg(*v, Variable, indexes)});
+      aliasTableMap.at(key)->insert({indexes, aliasArg(*v, Variable, indexes)});
+    else if (aliasTableMap.count(key, indexes) == 0)
+      aliasTableMap.at(key)->insert({indexes, aliasArg(*v, Variable, indexes)});
   }
 
   inline aliasArg *getAliasArg(const VarDecl *v) {
@@ -136,7 +203,14 @@ private:
     const NamedDecl *key = getKey(v);
     if (aliasTableMap.count(key) == 0)
       return nullptr;
-    return &aliasTableMap.at(key).at(std::vector<int>());
+    if (std::holds_alternative<aliasArg *>(*aliasTableMap.at(key)))
+      return std::get<aliasArg *>(*aliasTableMap.at(key));
+    else {
+      // TODO: Remove this later (if no issues arrise)
+      llvm::errs() << "Error in getAliasArg\n";
+      v->dump();
+      return nullptr;
+    }
   }
   void getReferencesAliases(const VarDecl *,
                             std::unordered_set<const VarDecl *> &) const;
