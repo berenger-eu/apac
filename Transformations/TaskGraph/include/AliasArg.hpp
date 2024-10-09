78dc4d416d8db3524fedd703bf2034438482eb2e
Julien <julien.gaupp@etu.unistra.fr>
2024-10-09 16:29:03 +0000

    moved dump functions

--- Transformations/TaskGraph/include/AliasArg.hpp
+++ Transformations/TaskGraph/include/AliasArg.hpp
@@ -29,45 +29,7 @@ struct aliasArg {
   aliasArg(const clang::VarDecl &decl, AliasType t,
            std::vector<int> indexes = std::vector<int>())
       : declaration(decl), type(t), indexes(indexes) {}
-  std::string varAsString() const {
-    std::stringstream ssRes;
-    ssRes << declaration.getNameAsString();
-    for (const auto &index : indexes)
-      ssRes << "[" << index << "]";
-    return ssRes.str();
-  }
-  std::string dumpAsStr() const {
-    std::stringstream ssRes;
-    ssRes << "AliasArg: " << varAsString() << "\n";
-    switch (type) {
-    case Reference:
-      ssRes << "Type: Reference\n";
-      break;
-    case Pointer:
-      ssRes << "Type: Pointer\n";
-      break;
-    case Variable:
-      ssRes << "Type: Variable\n";
-      break;
-    default:
-      ssRes << "Type: Unknown\n";
-      break;
-    }
-    ssRes << "Pointers: ";
-    for (const auto &ptr : pointers)
-      ssRes << ptr->declaration.getNameAsString() << " ";
-    ssRes << "\n";
-    ssRes << "References: ";
-    for (const auto &ref : references)
-      ssRes << ref->declaration.getNameAsString() << " ";
-    ssRes << "\n";
-    if (type == Reference || type == Pointer) {
-      ssRes << "Aliased: ";
-      for (const auto &aliased : aliased)
-        ssRes << aliased->declaration.getNameAsString() << " ";
-      ssRes << "\n";
-    }
-    return ssRes.str();
-  }
+  std::string varAsString() const;
+  std::string dumpAsStr() const;
   inline void dump() const { llvm::errs() << dumpAsStr(); }
 };
