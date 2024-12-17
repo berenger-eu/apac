c013f130d9f58a9879669a737689dbe1deaa250d
Julien Gaupp <julien.gaupp@inria.fr>
2024-12-17 16:48:17 +0000

    Duplication (ok) and call modifications (not finished)

---
+++ Transformations/DuplicateFunctions/include/duplicateFunctions.hpp
@@ -0,0 +1,19 @@
+#include "ASTChangeNameVisitor.hpp"
+#include "ASTDuplicateVisitor.hpp"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/Basic/Diagnostic.h"
+#include "clang/Basic/FileManager.h"
+#include "clang/Basic/TargetInfo.h"
+#include "clang/Basic/TargetOptions.h"
+#include <iostream>
+
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Lex/Preprocessor.h"
+#include "clang/Parse/ParseAST.h"
+
+#include "clang/Rewrite/Frontend/Rewriters.h"
+
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+
+#include "llvm/Support/raw_ostream.h"
