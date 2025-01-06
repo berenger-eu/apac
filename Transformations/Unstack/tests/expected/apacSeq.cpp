8e31f2823bfa45f79087c255457c12e5fcbfc51f
Julien Gaupp <julien.gaupp@inria.fr>
2025-01-06 09:43:28 +0000

    added stacked functions calls in apacSeq test in Unstack

--- Transformations/Unstack/tests/expected/apacSeq.cpp
+++ Transformations/Unstack/tests/expected/apacSeq.cpp
@@ -1,11 +1,11 @@
-void func_apacSeq(long long n) {
-  int a;
-  a = 4;
-}
 int f(int a) {
   int c;
   c = a + 1;
   return c;
+}
+void func_apacSeq(long long n) {
+  int a;
+  a = f(f(a));
 }
 int main() {
   long n;
