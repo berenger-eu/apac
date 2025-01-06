fb87d37423837983642e70cc0e876b1c8c05229d
Julien Gaupp <julien.gaupp@inria.fr>
2025-01-06 11:48:37 +0000

    Constify ignores apacSeq

--- Transformations/Constify/include/ASTConstifyVisitor.hpp
+++ Transformations/Constify/include/ASTConstifyVisitor.hpp
@@ -6,6 +6,12 @@ public:
   ASTConstifyVisitor(Rewriter &R, SymTab &SymTableIn)
       : TheRewriter(R), SymT(SymTableIn) {};
   inline bool VisitStmt(Stmt *) { return true; };
+  bool TraverseFunctionDecl(FunctionDecl *fDecl) {
+    if (fDecl->getNameAsString().find("_apacSeq") == std::string::npos) {
+      return RecursiveASTVisitor::TraverseFunctionDecl(fDecl);
+    }
+    return true;
+  }
   bool VisitCXXMethodDecl(CXXMethodDecl *);
   bool VisitBinaryOperator(BinaryOperator *);
   bool VisitUnaryOperator(UnaryOperator *);
