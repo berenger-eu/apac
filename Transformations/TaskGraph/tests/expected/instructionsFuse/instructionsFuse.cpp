e3501a75ca54afee5ad6738c9f31501338d609a2
Julien Gaupp <julien.gaupp@inria.fr>
2025-01-31 13:41:53 +0000

    fixed output for instructionsFuse test

--- Transformations/TaskGraph/tests/expected/instructionsFuse/instructionsFuse.cpp
+++ Transformations/TaskGraph/tests/expected/instructionsFuse/instructionsFuse.cpp
@@ -1,18 +1,35 @@
+#include <cstring>
+#include <omp.h>
+const int nb_cores = omp_get_max_threads();
+const int parallel_depth = ffs(nb_cores); // log2(nb_cores);
+int __apac_depth = 0;
+#pragma omp threadprivate(__apac_depth)
+const static int __apac_depth_max = parallel_depth;
 int main() {
-  int i, j;
-#pragma omp task depend(inout : i)
-  { i = 4; }
-#pragma omp task depend(inout : j)
-  { j = 4; }
-  int c;
-#pragma omp task depend(inout : c)
-  { c = 5; }
-#pragma omp task depend(in : i, j, c)
-  {
-    c = i + j;
-    c = i - j;
-    c = 2 * c;
-    c = 2 * c + j - i;
+  int __apac_depth_local = __apac_depth;
+  int __apac_depth_ok = (__apac_depth_local < __apac_depth_max);
+  if (__apac_depth_ok) {
+    int i, j;
+#pragma omp taskgroup
+    {
+#pragma omp task default(shared) depend(inout : i)
+      { i = 4; }
+#pragma omp task default(shared) depend(inout : j)
+      { j = 4; }
+      int c;
+#pragma omp task default(shared) depend(inout : c)
+      { c = 5; }
+#pragma omp task default(shared) depend(in : i, j, c)
+      {
+        c = i + j;
+        c = i - j;
+        c = 2 * c;
+        c = 2 * c + j - i;
+      }
+      ;
+    }
+    return 0;
+  } else {
+    return main_apacSeq();
   }
-  return 0;
 }
