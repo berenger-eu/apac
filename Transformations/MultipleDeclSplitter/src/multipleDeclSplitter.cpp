0cb9295c67c82db4ae38d392f33b0d80b3355295
Julien Gaupp <julien.gaupp@inria.fr>
2025-04-07 12:25:44 +0000

    changed variale name and added some comments to explain how options work

--- Transformations/MultipleDeclSplitter/src/multipleDeclSplitter.cpp
+++ Transformations/MultipleDeclSplitter/src/multipleDeclSplitter.cpp
@@ -54,9 +54,13 @@ bool MultipleDeclSplitterHandler::run(int argc, const char **argv) {
     exit(1);
   }
   llvm::cl::OptionCategory ToolingSampleCategory("Tooling Sample");
-  llvm::cl::opt<std::string> ASTDumpFilter("main");
-  llvm::cl::opt<std::string> ASTDumpFilterIgnore("ignore");
-  llvm::cl::opt<std::string> ASTDumpFilterFunction("functions");
+  // The following lines are used to define the options that can be passed
+  // Template type for the type of the argument
+  // The init string is the name of the option
+  llvm::cl::opt<std::string> APACMainFilter("main");
+  llvm::cl::opt<std::string> APACIgnoreFilter("ignore");
+  llvm::cl::opt<std::string> APACFunctionFilter("functions");
+
   llvm::Expected<clang::tooling::CommonOptionsParser> option =
       CommonOptionsParser::create(argc, argv, ToolingSampleCategory,
                                   llvm::cl::OneOrMore);
@@ -68,8 +72,8 @@ bool MultipleDeclSplitterHandler::run(int argc, const char **argv) {
   // the helper newFrontendActionFactory to create a default factory that will
   // return a new MyFrontendAction object every time.
   // To further customize this, we could create our own factory class.
-  callParse(ASTDumpFilter.getValue(), ASTDumpFilterFunction.getValue(),
-            ASTDumpFilterIgnore.getValue(), mainName, functionsToIgnore,
+  callParse(APACMainFilter.getValue(), APACFunctionFilter.getValue(),
+            APACIgnoreFilter.getValue(), mainName, functionsToIgnore,
             functions);
   return tool.run(newFrontendActionFactory<MyFrontendAction>().get());
 }
