0a6dde0527168362c8772361519cdd241b21c83a
Julien Gaupp <julien.gaupp@inria.fr>
2025-04-09 16:12:00 +0000

    Unstack

--- Transformations/Unstack/src/unstack.cpp
+++ Transformations/Unstack/src/unstack.cpp
@@ -8,6 +8,7 @@ using namespace clang::tooling;
 using namespace unstack;
 std::string mainName;
 std::vector<std::string> functions, functionsToIgnore;
+std::queue<std::string> filesOutputExt;
 // Implementation of the ASTConsumer interface for reading an AST produced
 // by the Clang parser.
 class MyASTConsumer : public ASTConsumer {
@@ -53,37 +54,17 @@ private:
   Rewriter TheRewriter;
 };
 
-bool UnstackHandler::run(int argc, const char **argv) {
-  if (argc < 2) {
-    std::cerr << "Call with following format : ./gotoRet <file.cpp> "
-                 "[<file.cpp> ...]\n";
-    exit(1);
+bool UnstackHandler::run(
+    llvm::Expected<clang::tooling::CommonOptionsParser> &options,
+    std::vector<std::string> &filesInput, const std::string &mainFilterValue,
+    const std::string &functionsFilterValue,
+    const std::string &functionsIgnoreValue,
+    const std::vector<std::string> &filesOutput) {
+  callParse(mainFilterValue, functionsFilterValue, functionsIgnoreValue,
+            mainName, functionsToIgnore, functions);
+  for (auto file : filesOutput) {
+    filesOutputExt.push(file);
   }
-  llvm::cl::OptionCategory ToolingSampleCategory("Tooling Sample");
-  // The following lines are used to define the options that can be passed
-  // Template type for the type of the argument
-  // The init string is the name of the option
-  llvm::cl::opt<std::string> APACMainFilter("main");
-  llvm::cl::opt<std::string> APACIgnoreFilter("ignore");
-  llvm::cl::opt<std::string> APACFunctionFilter("functions");
-  llvm::Expected<clang::tooling::CommonOptionsParser> option =
-      CommonOptionsParser::create(argc, argv, ToolingSampleCategory,
-                                  llvm::cl::OneOrMore);
-
-  auto files = option->getSourcePathList();
-
-  clang::tooling::ClangTool tool(option->getCompilations(), files);
-  // ClangTool::run accepts a FrontendActionFactory, which is then used to
-  // create new objects implementing the FrontendAction interface. Here we use
-  // the helper newFrontendActionFactory to create a default factory that will
-  // return a new MyFrontendAction object every time.
-  // To further customize this, we could create our own factory class.
-  callParse(APACMainFilter.getValue(), APACFunctionFilter.getValue(),
-            APACIgnoreFilter.getValue(), mainName, functionsToIgnore,
-            functions);
+  clang::tooling::ClangTool tool(options->getCompilations(), filesInput);
   return tool.run(newFrontendActionFactory<MyFrontendAction>().get());
-}
-
-int main(int argc, const char **argv) {
-  return UnstackHandler::run(argc, argv);
 }
