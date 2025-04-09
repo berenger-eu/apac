af5a77548b25d7251c41f90b6ce6a6f23667b816
Julien Gaupp <julien.gaupp@inria.fr>
2025-04-09 16:36:24 +0000

    gotoRet

--- Transformations/GotoTransfo/include/gotoRet.hpp
+++ Transformations/GotoTransfo/include/gotoRet.hpp
@@ -19,74 +19,12 @@
 
 class GotoRetHandler {
 public:
-  static bool run(int argc, const char **argv);
+  static bool
+  run(llvm::Expected<clang::tooling::CommonOptionsParser> &options,
+      std::vector<std::string> &filesInput,
+      const std::string &mainValue = std::string(),
+      const std::string &functionsValue = std::string(),
+      const std::string &ignoreValue = std::string(),
+
+      const std::vector<std::string> &filesOutput = std::vector<std::string>());
 };
-
-std::string gotoHeader = R"(
-#include <memory>
-#include <type_traits>
-#include <utility>
-
-template <class T>
-struct wrapper_natif {
-  T data;
-
-public:
-  wrapper_natif() = default;
-
-  template <typename... Args>
-  wrapper_natif(Args &&...args) : data(std::forward<Args>(args)...) {}
-
-  T &operator=(const T &other) {
-    data = other;
-    return data;
-  }
-
-  T &operator=(T &&other) {
-    data = std::move(other);
-    return data;
-  }
-
-  T &operator()() { return data; }
-
-  const T &operator()() const { return data; }
-
-  T &operator*() { return data; }
-
-  const T &operator->() const { return data; }
-};
-
-template <class T> struct wrapper_construct : public T {
-  using T::T;
-
-  T &operator*() { return *this; }
-
-  const T &operator->() const { return *this; }
-};
-
-template <typename T> struct wrapper {
-  // If T is default constructible, type = T
-  // Otherwise, type = std::unique_ptr<T>
-  using type = std::conditional_t<
-      std::is_fundamental_v<T>, wrapper_natif<T>,
-      std::conditional_t<std::is_default_constructible_v<T>,
-                         wrapper_construct<T>, std::unique_ptr<T>>>;
-};
-
-// Helper alias to avoid writing "typename wrapper<T>::type" everywhere
-template <typename T> using wrapper_t = typename wrapper<T>::type;
-
-// 2. The build_wrapper function that constructs the appropriate type.
-template <typename T, typename... Args>
-wrapper_t<T> build_wrapper(Args &&...args) {
-  // If T is default constructible, just call its constructor
-  if constexpr (std::is_fundamental_v<T>) {
-    return wrapper_natif<T>(std::forward<Args>(args)...);
-  } else if constexpr (std::is_default_constructible_v<T>) {
-    return wrapper_construct<T>(std::forward<Args>(args)...);
-  } else {
-    // Otherwise, build a std::unique_ptr<T>
-    return std::make_unique<T>(std::forward<Args>(args)...);
-  }
-}
-)";
--- Transformations/GotoTransfo/src/gotoRet.cpp
+++ Transformations/GotoTransfo/src/gotoRet.cpp
@@ -1,5 +1,5 @@
 #include "gotoRet.hpp"
-
+#include "gotoRetHeader.hpp"
 static llvm::cl::OptionCategory ToolingSampleCategory("Tooling Sample");
 
 using namespace clang;
@@ -36,6 +36,8 @@ void printTextToFiles(Rewriter &TheRewriter, const FileID &file) {
 }
 std::string mainName;
 std::vector<std::string> functions, functionsToIgnore;
+std::queue<std::string> filesOutputExt;
+
 // Implementation of the ASTConsumer interface for reading an AST produced
 // by the Clang parser.
 class MyASTConsumer : public ASTConsumer {
@@ -82,33 +84,17 @@ private:
   Rewriter TheRewriter;
 };
 
-bool GotoRetHandler::run(int argc, const char **argv) {
-  if (argc < 2) {
-    std::cerr << "Call with following format : ./gotoRet <file.cpp> "
-                 "[<file.cpp> ...]\n";
-    exit(1);
+bool GotoRetHandler::run(
+    llvm::Expected<clang::tooling::CommonOptionsParser> &options,
+    std::vector<std::string> &filesInput, const std::string &mainFilterValue,
+    const std::string &functionsFilterValue,
+    const std::string &functionsIgnoreValue,
+    const std::vector<std::string> &filesOutput) {
+  callParse(mainFilterValue, functionsFilterValue, functionsIgnoreValue,
+            mainName, functionsToIgnore, functions);
+  for (auto file : filesOutput) {
+    filesOutputExt.push(file);
   }
-  llvm::cl::opt<std::string> APACMainFilter("main");
-  llvm::cl::opt<std::string> APACIgnoreFilter("ignore");
-  llvm::cl::opt<std::string> APACFunctionFilter("functions");
-  llvm::Expected<clang::tooling::CommonOptionsParser> option =
-      CommonOptionsParser::create(argc, argv, ToolingSampleCategory,
-                                  llvm::cl::OneOrMore);
-
-  auto files = option->getSourcePathList();
-
-  clang::tooling::ClangTool tool(option->getCompilations(), files);
-  // ClangTool::run accepts a FrontendActionFactory, which is then used to
-  // create new objects implementing the FrontendAction interface. Here we use
-  // the helper newFrontendActionFactory to create a default factory that will
-  // return a new MyFrontendAction object every time.
-  // To further customize this, we could create our own factory class.
-  callParse(APACMainFilter.getValue(), APACFunctionFilter.getValue(),
-            APACIgnoreFilter.getValue(), mainName, functionsToIgnore,
-            functions);
+  clang::tooling::ClangTool tool(options->getCompilations(), filesInput);
   return tool.run(newFrontendActionFactory<MyFrontendAction>().get());
-}
-
-int main(int argc, const char **argv) {
-  return GotoRetHandler::run(argc, argv);
 }
